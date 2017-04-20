0ff59c70ef92b30fb8116207ed5e22d0fa72b674
DearZack <zack4work@gmail.com>
2017-04-20 23:17:08 +0800

    读取配置文件

--- src/main/java/io/github/dearzack/controller/HelloController.java
+++ src/main/java/io/github/dearzack/controller/HelloController.java
@@ -1,5 +1,6 @@
 package io.github.dearzack.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${cupSize}")//通过注解来获取application.properties配置文件中的属性
+    private String cupSize;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String say() {
-        return "Hello Spring Boot!!";
+        return "Hello Spring Boot!!" + cupSize;
     }
 }
